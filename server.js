2b35c27ffba8ea60cf784c6d8cbefb8ec12612ad
tswirski <tomaszswirski@wp.pl>
2019-04-07 15:16:46 +0200

    added passport authorization

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require('express');
+const session = require('express-session')
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const morgan = require('morgan');
-const { PORT } = require('./constants');
+const { PORT, SESSION_OPTIONS } = require('./constants');
 const rootRouter = require('./rootRouter')
 
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.static('public'));
+
 app.use(bodyParser.json());
+app.use(session(SESSION_OPTIONS));
+
+app.use(passport.initialize())
+app.use(passport.session());
+
+app.get('/', (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.redirect('/auth/github');
+  }
+});
+
+app.use(express.static('public'));
 app.use(rootRouter);
 
 app.set('port', PORT);
@@ -16,3 +32,4 @@ app.set('port', PORT);
 app.listen(app.get('port'), () => {
   console.info( `express app running on port: ${app.get('port')}`);
 });
+
